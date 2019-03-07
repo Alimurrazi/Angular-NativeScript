b52e9d6ac567836bd0f7208c951d10a4551d32d1
Alimur Razi Rana <alimur.razi@selise.ch>
2019-03-07 19:48:09 +0600

    page written

--- autogrillDemo/src/app/home/home.component.ts
+++ autogrillDemo/src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+    items;
 
     constructor() {
         // Use the component constructor to inject providers.
@@ -14,5 +15,12 @@ export class HomeComponent implements OnInit {
 
     ngOnInit(): void {
         // Init your component properties here.
+
+      this.items = new Array(
+          {food:"Bites and Nibbles Items, Prawn Tempura with wasabi mayo", topping:"No Toppings", amount: "1", money: "440 TK"},
+          {food:"Bites and Nibbles Items, Prawn Tempura with wasabi mayo", topping:"No Toppings", amount: "1", money: "440 TK"},
+          {food:"Bites and Nibbles Items, Prawn Tempura with wasabi mayo", topping:"No Toppings", amount: "1", money: "440 TK"}
+      )
+
     }
 }
